167b845c9e809e7afd1cf1412107044aeb6e73b9
adewale-adeyelu <adewaleadeyelu17@gmail.com>
2026-03-25 10:58:46 +0100

    changed a file name

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("./routes/landing/Route.tsx"), 
+  index("./routes/landing/route.tsx"), 
   route("login", "routes/auth/SignIn.tsx"),
   route("signup", "routes/auth/SignUp.tsx"),
   route("forgotpassword", "routes/auth/ForgotPassword.tsx"),
