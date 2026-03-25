4e25456aa806eac45a70c996ceeaca477ae4ec62
adewale-adeyelu <adewaleadeyelu17@gmail.com>
2026-03-25 11:21:58 +0100

    added a space

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
+
 export default [
   index("./routes/landing/route.tsx"), 
   route("login", "routes/auth/SignIn.tsx"),
